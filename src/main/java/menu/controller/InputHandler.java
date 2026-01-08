f95403b0068713d1e398c9e06cc36dd8fc6bb4bc
sumi <lsmkoo03@naver.com>
2026-01-08 16:41:30 +0900

    feat: 메뉴 검증 기능 구현

--- src/main/java/menu/controller/InputHandler.java
+++ src/main/java/menu/controller/InputHandler.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class InputHandler {
     public static String getCoachName() {
         while (true) {
-            String input = InputView.readLine("점심 메뉴 추천을 시작합니다.\n" + "\n" + "코치의 이름을 입력해 주세요. (, 로 구분)");
+            String input = InputView.readLine("점심 메뉴 추천을 시작합니다.\n\n" + "코치의 이름을 입력해 주세요. (, 로 구분)");
             try {
                 InputValidator.validateCoachName(input);
                 return input;
@@ -19,29 +19,33 @@ public class InputHandler {
         }
     }
 
-    public static void getMenu(String coachName) {
-        String coachs[] = coachName.split(",");
+    public static List<List<String>> getMenu(String coachName) {
+        String[] coaches = coachName.split(",");
         List<List<String>> coachMenuList = new ArrayList<>();
-        for (String coach : coachs) {
+
+        for (String coach : coaches) {
             while (true) {
-                String input = InputView.readLine(coach + "(이)가 못 먹는 메뉴를 입력해 주세요.");
-                coachMenuList.add(validateAndConversionMenu(coach, input));
+                String input = InputView.readLine("\n" + coach + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+                try {
+                    List<String> bundle = validateAndConversionMenu(coach, input);
+                    coachMenuList.add(bundle);
+                    break;
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }
+        return coachMenuList;
     }
 
     private static List<String> validateAndConversionMenu(String coach, String menu) {
+        InputValidator.validateMenu(menu);
         List<String> coachAndMenuBundle = new ArrayList<>();
         coachAndMenuBundle.add(coach);
-        try {
-            InputValidator.validateMenu(menu);
-            String menus[] = menu.split(",");
-            for (String oneMenu : menus) {
-                coachAndMenuBundle.add(oneMenu);
-            }
-            return coachAndMenuBundle;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+
+        String[] menus = menu.split(",");
+        for (String oneMenu : menus) {
+            coachAndMenuBundle.add(oneMenu);
         }
         return coachAndMenuBundle;
     }
--- src/main/java/menu/util/InputValidator.java
+++ src/main/java/menu/util/InputValidator.java
@@ -1,5 +1,7 @@
 package menu.util;
 
+import java.util.List;
+
 public class InputValidator {
     public static void validateCoachName(String input) {
         String coachName[] = input.split(",");
@@ -9,6 +11,24 @@ public class InputValidator {
         for (String coach : coachName) {
             if (coach.length() < 2 || coach.length() > 4) {
                 throw new IllegalArgumentException("[ERROR] 코치의 이름은 2글자 이상 4글자 이하로 입력해야 합니다.");
+            }
+        }
+    }
+
+    public static void validateMenu(String menu) {
+        List<String> japenMenus = MenuManager.getJapenMenus();
+        List<String> koreaMenus = MenuManager.getKoreaMenus();
+        List<String> chinaMenus = MenuManager.getChinaMenus();
+        List<String> asianMenus = MenuManager.getAsianMenus();
+        List<String> westernMenus = MenuManager.getWesternMenus();
+        String menus[] = menu.split(",");
+
+        if (menus.length < 0 || menus.length > 2) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 0개 이상 2개 이하로 입력해야 합니다.");
+        }
+        for (String oneMenu : menus) {
+            if (!(japenMenus.contains(oneMenu) || koreaMenus.contains(oneMenu) || chinaMenus.contains(oneMenu) || asianMenus.contains(oneMenu) || westernMenus.contains(oneMenu))) {
+                throw new IllegalArgumentException("[ERROR] 올바른 메뉴 이름을 입력해야 합니다.");
             }
         }
     }
